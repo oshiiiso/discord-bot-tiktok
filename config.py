ada5222e6a98a964ddc932b4aea6264c8b31434b
oshiiiso <317916529+oshiiiso@users.noreply.github.com>
2026-07-04 16:31:55 +0900

    WEBHOOKからDiscordBotに変更

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 DEBUG_MODE = os.getenv("DEBUG_MODE", "false").lower() == "true"
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
